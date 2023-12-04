0be383bedb8296efc036fac5daaff4ba6ecaf8ae
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-12-04 14:31:21 +0200

    persisting auth state, and logout

--- react-tsc/src/components/Login.tsx
+++ react-tsc/src/components/Login.tsx
@@ -5,21 +5,22 @@ export interface IUser {
 }
 
 export const Login = () => {
-  const [loggedUser, setLoggedUser] = useState<IUser | null>(null);
-
-  // --- PERSISTED STATE ---
-  // () => {
-  // const userInSessionStorage = JSON.parse(sessionStorage.getItem('ts-reactDemoUser'));
-  // if(userInSessionStorage){
-  //   setLoggedUser(userInSessionStorage);
-  // }
-  // return null;
-  // }
+  const [loggedUser, setLoggedUser] = useState<IUser | null>(() => {
+    const userInSessionStorage = JSON.parse(sessionStorage.getItem('ts-reactDemoUser')!);
+    if (userInSessionStorage) {
+      return userInSessionStorage;
+    }
+    return null;
+  });
 
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
-  const onLoginHandler = (e: React.MouseEvent<HTMLButtonElement>): void => {
+  const onLogoutHandler = () => {
+    setLoggedUser(null);
+  }
+
+  const onLoginHandler = (e: React.MouseEvent<HTMLFormElement>): void => {
     e.preventDefault();
 
     sessionStorage.setItem('ts-reactDemoUser', JSON.stringify({ email, password }));
@@ -28,6 +29,9 @@ export const Login = () => {
       email,
       password
     });
+
+    setEmail('');
+    setPassword('');
   }
 
   const onEmailChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
@@ -42,11 +46,14 @@ export const Login = () => {
     <section className="view-section">
       <h1>Login</h1>
       {loggedUser
-        ? <p>you logged with {loggedUser.email}</p>
-        : <form className="login-form">
+        ? <>
+          <p>you logged with {loggedUser.email}</p>
+          <button onClick={onLogoutHandler}>Logout</button>
+        </>
+        : <form className="login-form" onSubmit={onLoginHandler}>
           <input type="email" name="email" placeholder="email" value={email} onChange={onEmailChange} />
           <input type="password" name="password" placeholder="password" value={password} onChange={onPasswordChange} />
-          <button onClick={onLoginHandler}>Login</button>
+          <button>Login</button>
         </form>}
 
     </section>
