0a2ae818d9a2c333b5dd18cb3dec5ec488802f9d
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-12-13 13:35:52 +0200

    delete dist from repo

--- express-tsc/dist/model/service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAllCars = void 0;
-const data_1 = require("./data");
-function getAllCars() {
-    return data_1.cars;
-}
-exports.getAllCars = getAllCars;
-//# sourceMappingURL=service.js.map
