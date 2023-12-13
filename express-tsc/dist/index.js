db5a2594d1143872c10b37179db84a4cb660a93a
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-12-13 11:34:20 +0200

    ejs config in express with typescript

--- express-tsc/dist/index.js
+++ express-tsc/dist/index.js
@@ -5,10 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const express_ejs_layouts_1 = __importDefault(require("express-ejs-layouts"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
+app.use(express_1.default.urlencoded({ extended: false })); // forms
+app.use(express_ejs_layouts_1.default);
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+// routing table
 app.get('/', (req, res) => {
-    res.send({ message: 'hello from express with tsc' });
+    res.render('home', {});
+});
+app.get('/catalog', (req, res) => {
+    res.render('catalog', {});
+});
+app.get('/about', (req, res) => {
+    res.render('about', {});
 });
 app.listen(6161, () => console.log('server listen on port 6161'));
 //# sourceMappingURL=index.js.map
--- express-tsc/src/index.ts
+++ express-tsc/src/index.ts
@@ -1,11 +1,28 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import expressEjsLayouts from 'express-ejs-layouts';
 
 const app = express();
+
 app.use(cors());
 
+app.use(express.urlencoded({ extended: false })); // forms
+app.use(expressEjsLayouts);
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+
+
+// routing table
 app.get('/', (req: Request, res: Response) => {
-    res.send({ message: 'hello from express with tsc' });
+    res.render('home', { });
+});
+
+app.get('/catalog', (req: Request, res: Response) => {
+    res.render('catalog', { });
+});
+
+app.get('/about', (req: Request, res: Response) => {
+    res.render('about', { });
 });
 
 app.listen(6161, () => console.log('server listen on port 6161'));
