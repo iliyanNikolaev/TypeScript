70dac6254483b96114dcb0b4e5ef3e5914b105b0
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-12-24 15:10:26 +0200

    express cookies demo

--- nodejs-http-serverModule/build/js/index.js
+++ nodejs-http-serverModule/build/js/index.js
@@ -2,10 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = require("http");
 const server = (0, http_1.createServer)((req, res) => {
-    console.log(req.method);
-    console.log(req.headers);
-    console.log(req.url);
-    res.write('hello world');
-    res.end();
+    if (req.url == '/') {
+        let visited = 1;
+        if (req.headers.cookie) {
+            const cookies = Object.fromEntries(req.headers.cookie.split(';')
+                .map((c) => c.trim())
+                .map((c) => c.split('=')));
+            if (cookies.visited) {
+                visited = Number(cookies.visited) + 1;
+            }
+        }
+        res.writeHead(200, {
+            'set-cookie': 'visited=' + visited
+        });
+        res.write(`<p>you visited this site ${visited} times</p>`);
+        res.end();
+    }
+    else {
+        res.writeHead(404);
+        res.end();
+    }
 });
 server.listen('3002', () => console.log('server runing on port 3002'));
--- nodejs-http-serverModule/src/index.ts
+++ nodejs-http-serverModule/src/index.ts
@@ -1,9 +1,28 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http';
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+    if (req.url == '/') {
+        let visited = 1;
+        if (req.headers.cookie) {
+            const cookies = Object.fromEntries(
+                req.headers.cookie.split(';')
+                    .map((c: string) => c.trim())
+                    .map((c: string) => c.split('='))
+            );
 
-    res.write('hello world');
-    res.end();
+            if(cookies.visited) {
+                visited = Number(cookies.visited) + 1;
+            }
+        }
+        res.writeHead(200, {
+            'set-cookie': 'visited=' + visited
+        })
+        res.write(`<p>you visited this site ${visited} times</p>`);
+        res.end();
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
 });
 
 server.listen('3002', () => console.log('server runing on port 3002'))
