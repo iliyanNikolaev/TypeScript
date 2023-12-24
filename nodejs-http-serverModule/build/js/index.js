23e23508a01e9ba8afcfb59d4327da02ab8e78ee
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-12-24 16:55:45 +0200

    node js and typescript cookies and sessions demo

--- nodejs-http-serverModule/build/js/index.js
+++ nodejs-http-serverModule/build/js/index.js
@@ -3,20 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = require("http");
 const server = (0, http_1.createServer)((req, res) => {
     if (req.url == '/') {
-        let visited = 1;
-        if (req.headers.cookie) {
-            const cookies = Object.fromEntries(req.headers.cookie.split(';')
-                .map((c) => c.trim())
-                .map((c) => c.split('=')));
-            if (cookies.visited) {
-                visited = Number(cookies.visited) + 1;
-            }
-        }
-        res.writeHead(200, {
-            'set-cookie': 'visited=' + visited
-        });
-        res.write(`<p>you visited this site ${visited} times</p>`);
-        res.end();
+        action(req, res);
     }
     else {
         res.writeHead(404);
@@ -24,3 +11,24 @@ const server = (0, http_1.createServer)((req, res) => {
     }
 });
 server.listen('3002', () => console.log('server runing on port 3002'));
+const parseCookie = (req) => {
+    if (req.headers.cookie) {
+        const cookies = Object.fromEntries(req.headers.cookie.split(';')
+            .map((c) => c.trim())
+            .map((c) => c.split('=')));
+        return cookies;
+    }
+    return {};
+};
+let sessions = {};
+const action = (req, res) => {
+    const cookies = parseCookie(req);
+    const sessionId = cookies.sessionId || (Math.random() * 999999).toString(16);
+    let session = sessions[sessionId] || { name: 'user' + sessionId };
+    sessions[sessionId] = session;
+    res.writeHead(200, {
+        'set-cookie': `sessionId=${sessionId}; httpOnly`
+    });
+    res.write(`<p>You are user ${sessionId}</p>`);
+    res.end();
+};
--- nodejs-http-serverModule/src/index.ts
+++ nodejs-http-serverModule/src/index.ts
@@ -2,27 +2,50 @@ import { createServer, IncomingMessage, ServerResponse } from 'http';
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
     if (req.url == '/') {
-        let visited = 1;
-        if (req.headers.cookie) {
-            const cookies = Object.fromEntries(
-                req.headers.cookie.split(';')
-                    .map((c: string) => c.trim())
-                    .map((c: string) => c.split('='))
-            );
-
-            if(cookies.visited) {
-                visited = Number(cookies.visited) + 1;
-            }
-        }
-        res.writeHead(200, {
-            'set-cookie': 'visited=' + visited
-        })
-        res.write(`<p>you visited this site ${visited} times</p>`);
-        res.end();
+        action(req, res);
     } else {
         res.writeHead(404);
         res.end();
     }
 });
 
-server.listen('3002', () => console.log('server runing on port 3002'))
+server.listen('3002', () => console.log('server runing on port 3002'));
+
+type CookieObjType = { sessionId?: string };
+
+const parseCookie = (req: IncomingMessage): CookieObjType => {
+    if (req.headers.cookie) {
+        const cookies = Object.fromEntries(
+            req.headers.cookie.split(';')
+                .map((c: string) => c.trim())
+                .map((c: string) => c.split('='))
+        )
+        return cookies
+    }
+
+    return {}
+}
+
+type SessionsType = Record<string, SessionType>
+
+type SessionType = {
+    name: string
+}
+
+let sessions: SessionsType = {};
+
+const action = (req: IncomingMessage, res: ServerResponse): void => {
+    const cookies = parseCookie(req);
+
+    const sessionId = cookies.sessionId || (Math.random() * 999999).toString(16);
+    
+    let session: SessionType = sessions[sessionId] || { name: 'user'+ sessionId };
+
+    sessions[sessionId] = session;
+
+    res.writeHead(200, {
+        'set-cookie': `sessionId=${sessionId}; httpOnly`
+    });
+    res.write(`<p>You are user ${sessionId}</p>`);
+    res.end();
+}
